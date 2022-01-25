3b5b2599b554e68ec26b878ef88db72efe7433b4
Eduards <eduards.polis2@gmail.com>
2022-01-25 09:08:25 +0200

    Pievienota kriteriju vertiba un to ievade

--- src/gitUzdevumi/uzd1.java
+++ src/gitUzdevumi/uzd1.java
@@ -1,22 +1,48 @@
 package gitUzdevumi;
 
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
+
 
 public class uzd1 {
 
 	public static void main(String[] args) {
-		String[][] cilveki = null;
-		String skaits;
+		int studSk, kritSk;
+		Scanner scan = new Scanner(System.in);
 		
-		skaits = JOptionPane.showInputDialog(null, "Cik cilvekus velies ievadit");
+		do {
+			System.out.println("Cik studentiem aprekinasi gala vertejumu");
+			studSk = scan.nextInt();
+		}while(studSk<1);
+		String [] studenti = new String[studSk];
 
-		for(int i=0; i<skaits.length(); i++) {
-			for(int j=0; j<skaits.length(); j++) {
-			cilveki[i][i] = JOptionPane.showInputDialog(null, "Ka sauc "+i+" cilveku");
+		do {
+			System.out.println("Kads bus kriteriju skaits?");
+			kritSk = scan.nextInt();
+		}while(kritSk<1);
+		String[] kriteriji = new String[kritSk];
+		int[] kriterijaSvars = new int[kritSk];
+		int [][] kriterijaVertejums = new int[studSk][];
+		double[] semestraVertejums = new double[studSk];
+		
+		//Ievada studentu vardus un uzvardus
+		for(int i=0; i<studenti.length; i++) {
+			System.out.println("Ievadi "+(i+1)+". studentu");
+			studenti[i] = scan.next();
 			
-			}
+		}	
+		//define kriterijus
+		int maxSvars=100;
+		for(int i=0; i<kriteriji.length; i++) {
+			System.out.println("Ievadi "+(i+1)+". kriteriju");
+			kriteriji[i] = scan.next();
+			
+			do {
+				System.out.println("Ievadi "+kriteriji[i]+" svaru");
+				kriterijaSvars[i] = scan.nextInt();
+			}while(kriterijaSvars[i]>100 || kriterijaSvars[i]<1);
+			maxSvars -=kriterijaSvars[i];
 		}
-		
-	}
-
+		}
 }
