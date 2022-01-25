c0888f0cb837afdf3601d093784f0958dc287fce
Eduards <eduards.polis2@gmail.com>
2022-01-25 08:57:25 +0200

    Uztaisita studentu un kriteriju ievade

--- src/gitUzdevumi/uzd1.java
+++ src/gitUzdevumi/uzd1.java
@@ -5,11 +5,18 @@ import javax.swing.JOptionPane;
 public class uzd1 {
 
 	public static void main(String[] args) {
-		int[][] cilveki;
+		String[][] cilveki = null;
 		String skaits;
 		
 		skaits = JOptionPane.showInputDialog(null, "Cik cilvekus velies ievadit");
 
+		for(int i=0; i<skaits.length(); i++) {
+			for(int j=0; j<skaits.length(); j++) {
+			cilveki[i][i] = JOptionPane.showInputDialog(null, "Ka sauc "+i+" cilveku");
+			
+			}
+		}
+		
 	}
 
 }
